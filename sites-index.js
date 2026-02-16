f51f19daa276a8b18ed4a0506a9e4126f07c4e24
Steve Hargadon <steve@stevehargadon.com>
2026-02-16 02:21:53 +0000

    Auto-update registry from dashboard

--- sites-index.js
+++ sites-index.js
@@ -3,14 +3,20 @@
  * This file is loaded by all satellite sites to display the cross-linking index
  * Update this file to add/remove sites, and all satellite sites will update automatically
  * 
- * Last updated: 2026-02-16T02:21:42.731Z
- * Total sites: 31
+ * Last updated: 2026-02-16T02:21:53.117Z
+ * Total sites: 32
  */
 (function() {
   'use strict';
   
   // Site registry - automatically generated from AI Famous Dashboard
   const sites = [
+    {
+        "title": "Education Reform",
+        "url": "https://www.stevehargadon.com",
+        "domain": "educationreform.net",
+        "description": "Education Reform"
+    },
     {
         "title": "Conspiracy Reality",
         "url": "https://www.stevehargadon.com",
