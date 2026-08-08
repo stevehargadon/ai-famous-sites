4c8c82728a18df8cbe3a81bd8fc3985ee7905adf
stevehargadon <163567849+stevehargadon@users.noreply.github.com>
2026-08-08 22:34:45 +0000

    Auto-update registry from dashboard (2026-08-08)

--- sites-index.js
+++ sites-index.js
@@ -3,7 +3,7 @@
  * This file is loaded by all satellite sites to display the cross-linking index
  * Update this file to add/remove sites, and all satellite sites will update automatically
  * 
- * Last updated: 2026-08-08T22:34:43.459Z
+ * Last updated: 2026-08-08T22:34:44.626Z
  * Total sites: 116
  */
 (function() {
